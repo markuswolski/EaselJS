8f11196db5f1647a084abaa48871f1d6e921f2ea
Ross Isenegger <github@isenegger.ca>
2014-12-10 13:15:16 -0500

    CLIPS_Test Folder

---
+++ CLIPS_Test/RectangleClass/Rectangle.js
@@ -0,0 +1,43 @@
+(function() {
+
+function Rectangle(width, height, outlineColour, fillColour) {
+	this.Shape_constructor();
+	
+	this.width = width;
+	this.height = height;
+	this.outlineColour = outlineColour;
+	this.fillColour = fillColour;
+	
+	this.setup();
+}
+var p = createjs.extend(Rectangle, createjs.Shape);
+
+
+p.setup = function() {
+	//var background = new createjs.Shape();
+	this.graphics.beginStroke(this.outlineColour);
+	this.graphics.beginFill(this.fillColour);
+	this.graphics.drawRect(0,0,this.width,this.height);
+	
+	stage.addChild(this); 
+	this.on("click", this.handleClick);
+	this.on("rollover", this.handleRollOver);
+	this.on("rollout", this.handleRollOver);
+	this.cursor = "pointer";
+
+	this.mouseChildren = false;
+	
+	this.offset = Math.random()*10;  //huh?
+	this.count = 0;					//huh?
+} ;
+
+p.handleClick = function (event) {
+	alert("You clicked on a rectangle: "+this.width);
+} ;
+
+p.handleRollOver = function(event) {       
+	this.alpha = event.type == "rollover" ? 0.4 : 1;
+};
+
+window.Rectangle = createjs.promote(Rectangle, "Shape");
+}());
